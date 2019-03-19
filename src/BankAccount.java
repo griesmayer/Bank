ea0d49f73a2b5b690fd429351ba1b57054b1e524
Thomas Griesmayer <griesmayer@spengergasse.at>
2019-03-19 20:52:30 +0100

    BankAccount

---
+++ src/BankAccount.java
@@ -0,0 +1,29 @@
+public class BankAccount {
+	private int accountId;
+	private String name;
+	private double amount;
+	public int getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
+	
+}
